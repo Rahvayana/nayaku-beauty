bf8e59078b92420e58186c5ea3d9a7001c447eb3
fahrul sanjaya <fahrursanjaya@gmail.com>
2020-12-18 21:34:02 +0700

    bug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <div>
+                <img src="{{asset('images/logo.jpeg')}}" alt="" width="20%"  style="display: block; margin: auto;">
+            </div>   
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
